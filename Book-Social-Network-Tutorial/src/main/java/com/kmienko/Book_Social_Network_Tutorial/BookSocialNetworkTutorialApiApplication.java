57929417a72a8ed40a58c1b1ddff6e941e1f9702
kmienko <kamilmienko95@gmail.com>
2024-09-04 17:20:03 +0200

    spring initializr + readme update

---
+++ Book-Social-Network-Tutorial/src/main/java/com/kmienko/Book_Social_Network_Tutorial/BookSocialNetworkTutorialApiApplication.java
@@ -0,0 +1,13 @@
+package com.kmienko.Book_Social_Network_Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkTutorialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialNetworkTutorialApiApplication.class, args);
+	}
+
+}
---
+++ Book-Social-Network-Tutorial/src/test/java/com/kmienko/Book_Social_Network_Tutorial/BookSocialNetworkTutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmienko.Book_Social_Network_Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialNetworkTutorialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
