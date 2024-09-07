a2345c32399f8d802e3dde803d4af40999137efd
kmienko <kamilmienko95@gmail.com>
2024-09-07 17:42:26 +0200

    create role USER during startup
    restore JPA Auditing instead of JPA Repositories due to problem with @CreatedDate being null

--- Book-Social-Network-Tutorial/src/main/java/com/kmienko/Book_Social_Network_Tutorial/BookSocialNetworkTutorialApiApplication.java
+++ Book-Social-Network-Tutorial/src/main/java/com/kmienko/Book_Social_Network_Tutorial/BookSocialNetworkTutorialApiApplication.java
@@ -1,17 +1,29 @@
 package com.kmienko.Book_Social_Network_Tutorial;
 
+import com.kmienko.Book_Social_Network_Tutorial.role.Role;
+import com.kmienko.Book_Social_Network_Tutorial.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaAuditing
 @EnableAsync
 public class BookSocialNetworkTutorialApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookSocialNetworkTutorialApiApplication.class, args);
-	}
 
+	}
+		@Bean
+		public CommandLineRunner runner(RoleRepository roleRepository){
+			return args -> {
+				if (roleRepository.findByName("USER").isEmpty())
+						roleRepository.save(Role.builder().name("USER").build()
+					);
+			};
+		}
 }
