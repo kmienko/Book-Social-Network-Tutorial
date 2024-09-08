a36a57458f366be91476f8c2eef04e5f696f47df
kmienko <kamilmienko95@gmail.com>
2024-09-08 16:57:24 +0200

    custom exceptions and response validator

---
+++ Book-Social-Network-Tutorial/src/main/java/com/kmienko/Book_Social_Network_Tutorial/handler/ExceptionResponse.java
@@ -0,0 +1,22 @@
+package com.kmienko.Book_Social_Network_Tutorial.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
+
